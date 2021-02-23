e121ac6d59f93daf2c2038bde72e0c301876dc4f
NihwlCat <pedro.ra.nihwl@gmail.com>
2021-02-23 17:16:04 -0300

    Deletando dados do SQL

--- src/pedro/Program.java
+++ src/pedro/Program.java
@@ -1,34 +1,47 @@
 package pedro;
 
 import db.DB;
+import db.DbIntegrityException;
+
 import java.sql.*;
 
 public class Program {
     public static void main(String[] args) {
+
+        /*
+        * Elementos com chave estrangeira associadas a uma outra chave primária não
+        * podem ser excluidos. Exemplo: Departamentos que possuem vendedores não
+        * podem ser excluidos pois estaria gerando uma falha de integridade referencial.
+        */
+
         Connection conn;
         PreparedStatement st = null;
         try {
             conn = DB.getConn();
             st = conn.prepareStatement(
-                    "UPDATE seller "
-                    + "SET BaseSalary = BaseSalary + ?"
-                    + "WHERE"
-                    + "(DepartmentId = ?)");
-            // Banco de dados precisa de restrição para não atualizar todos os dados
+                    "DELETE FROM "
+                    +"department WHERE Id = ?"
+            );
 
-            st.setDouble(1, 200.0);
-            st.setInt(2, 2);
+             /*st = conn.prepareStatement(
+                    "INSERT INTO department (Name) VALUES (?) "
+            ); */
+
+
+            st.setInt(1, 5);
+
+            /*st.setString(1,"Teste1"); */
 
             int ra = st.executeUpdate();
 
             System.out.println("Done: " + ra);
 
         } catch (SQLException e){
-            e.printStackTrace();
+            throw new DbIntegrityException(e.getMessage());
         } finally {
             DB.closeStatement(st);
             DB.closeConn();
         }
-        
+
     }
 }
