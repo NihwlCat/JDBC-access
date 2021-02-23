e45cc2ff92c9f50d484ddce1269d8f03b64e3fc1
NihwlCat <pedro.ra.nihwl@gmail.com>
2021-02-23 16:24:34 -0300

    Inserindo dados no SQL com preparedStatement

--- src/pedro/Program.java
+++ src/pedro/Program.java
@@ -1,38 +1,51 @@
 package pedro;
 
 import db.DB;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
-        Statement statement = null; // Preparar consulta SQL
-        ResultSet rs = null; // Guardar resultado da consulta
-
+        PreparedStatement statement = null; // Preparar consulta SQL e inserir dados posteriormente.
         try {
             conn = DB.getConn();
-            statement = conn.createStatement();
-            rs = statement.executeQuery("select * from department"); // Entrar com comando SQL
 
-            while(rs.next()){
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("name"));
-            }
+            statement = conn.prepareStatement(
+                    "INSERT INTO seller "
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                    + "VALUES "
+                    + "(?, ?, ?, ?, ?)"); // Comandos SQL
+
+            // Sobrecarga de preparedStatement (comando_SQL,Statement RETURN_GENERATED_KEY)
+            // Retornar a chave gerada pela inserção
+
+            statement.setString(1,"Matheus Bichuette");
+            statement.setString(2,"bichuette@gmail.com");
+            statement.setDate(3, new Date(sdf.parse("22/04/1985").getTime()));
+            statement.setDouble(4,3000.97);
+            statement.setInt(5,4);
+
+            int rowsAffected = statement.executeUpdate(); // Executar atualização da tabela que retorna o número de linhas alteradas.
+            System.out.println("Done, " + rowsAffected);
+
+            /*
+            * ResultSet rs = sr.getGeneratedKeys();
+            * while (rs.next()){ ind id = rs.getInt(1) }
+            * Uma tabela auxiliar contendo uma coluna com os Ids
+            * */
+
+
         } catch (SQLException e){
             e.printStackTrace();
-
+        } catch (ParseException e){
+            e.printStackTrace();
         } finally {
-            DB.closeConn();
-            DB.closeResultSet(rs);
             DB.closeStatement(statement);
+            DB.closeConn();
         }
-
-
-
-
 
     }
 }
