1584932b7d10e848cc4e42b0f9b9d904ccf54be3
NihwlCat <pedro.ra.nihwl@gmail.com>
2021-02-23 15:50:44 -0300

    Aprendendo consulta ao SQL utilizando Statement e ResultSet

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -45,4 +43,24 @@ public class DB {
             throw new DBException(e.getMessage());
         }
     }
+
+    public static void closeStatement (Statement t){
+        if(t != null){
+            try {
+                t.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        }
+    }
+    public static void closeResultSet (ResultSet rs){
+        if(rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        }
+    }
+
 }
--- src/pedro/Program.java
+++ src/pedro/Program.java
@@ -3,10 +3,36 @@ package pedro;
 import db.DB;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
     public static void main(String[] args) {
-        Connection conn = DB.getConn();
-        DB.closeConn();
+        Connection conn = null;
+        Statement statement = null; // Preparar consulta SQL
+        ResultSet rs = null; // Guardar resultado da consulta
+
+        try {
+            conn = DB.getConn();
+            statement = conn.createStatement();
+            rs = statement.executeQuery("select * from department"); // Entrar com comando SQL
+
+            while(rs.next()){
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("name"));
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+
+        } finally {
+            DB.closeConn();
+            DB.closeResultSet(rs);
+            DB.closeStatement(statement);
+        }
+
+
+
+
+
     }
 }
